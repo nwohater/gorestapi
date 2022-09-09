2a8600f76e26a1ae91574436e97acc6baa5cd760
Brandon Lackey <44684885+nwohater@users.noreply.github.com>
2022-09-09 15:12:20 -0700

    struct

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
+// tutorial: https://www.youtube.com/watch?v=d_L64KT3SFM
 // rest api using gin framework
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"item"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "First Item", Completed: false},
+	{ID: "2", Item: "Second Item", Completed: false},
+	{ID: "2", Item: "Third Item", Completed: false},
+}
