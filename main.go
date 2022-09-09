157815974d0dfac62c96b2c74de688e2389dd53c
Brandon Lackey <44684885+nwohater@users.noreply.github.com>
2022-09-09 15:25:41 -0700

    add todo

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 // tutorial: https://www.youtube.com/watch?v=d_L64KT3SFM
 // rest api using gin framework
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type todo struct {
 	ID        string `json:"id"`
 	Item      string `json:"item"`
@@ -13,4 +19,26 @@ var todos = []todo{
 	{ID: "1", Item: "First Item", Completed: false},
 	{ID: "2", Item: "Second Item", Completed: false},
 	{ID: "2", Item: "Third Item", Completed: false},
+}
+
+func addTodo(c *gin.Context) {
+	var newTodo todo
+
+	if err := c.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+	c.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+func getTodos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
+	router.Run("localhost:8080")
 }
